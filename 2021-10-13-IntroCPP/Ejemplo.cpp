9e2fd66b79147c67017357957eae298c3abef743
Alejandro Baracaldo <lbaracaldo@unal.edu.co>
2021-10-13 11:47:58 -0500

    Primer Ejemplo

---
+++ 2021-10-13-IntroCPP/Ejemplo.cpp
@@ -0,0 +1,16 @@
+
+//CALCULAR LOS NÚMEROS PRIMOS ENTRE 1 Y 100
+
+//Incluir las librerías que se van a utilizar
+#include <iostream>
+#include <cmath>
+
+//main function
+int main(void)
+{
+  int m = 1;
+  int n = 100;
+    std::cout << "m: " << m << "\n";
+    std::cout << "n: " << n << "\n";
+  return 0;
+}
