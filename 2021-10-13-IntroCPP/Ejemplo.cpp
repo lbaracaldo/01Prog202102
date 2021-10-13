f09432d3d159492c952d132f3319222153c17bab
Alejandro Baracaldo <lbaracaldo@unal.edu.co>
2021-10-13 16:27:48 -0500

    Functions, If, Counter, Suma

--- 2021-10-13-IntroCPP/Ejemplo.cpp
+++ 2021-10-13-IntroCPP/Ejemplo.cpp
@@ -1,16 +1,34 @@
 
-//CALCULAR LOS NÚMEROS PRIMOS ENTRE 1 Y 100
+//CALCULAR LOS NÚMEROS PARES ENTRE 1 Y 100
 
 //Incluir las librerías que se van a utilizar
 #include <iostream>
 #include <cmath>
 
+void print_even(int nmin, int nmax); //Declaración
+
 //main function
 int main(void)
 {
-  int m = 1;
-  int n = 100;
-    std::cout << "m: " << m << "\n";
-    std::cout << "n: " << n << "\n";
+  const int m = 1;
+ const int n = 15;
+    
+ print_even(m,n);
+ print_even(m/2,2*n);
+ 
   return 0;
+}
+
+void print_even(int nmin, int nmax) // Implementación
+{
+  int suma = 0;
+  for(int ii = nmin; ii <= nmax; ii = ii+1){
+      //Si el número es par, entonces imprimir
+      if(ii%2 == 0){
+	std::cout << ii << " ";
+	suma = suma+ii;
+      }
+    }
+  std::cout << "\n";
+  std::cout << suma << " ";
 }
