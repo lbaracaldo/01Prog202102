35875a0e557d68a0055502035db9a15e3d1e6b91
Alejandro Baracaldo <lbaracaldo@unal.edu.co>
2021-10-18 22:46:00 -0500

    Inicialización de Variables

---
+++ 2021-10-15-Intro/Intro.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hola Mundo\n";
+  int a;
+  float x;
+  double y=0.0;
+  std::cout << "a = " << a << "\n";
+  std::cout << "x = " << x << "\n";
+  std::cout << "y = " << y << "\n";
+return 0;
+}
+
+//Edición: Escribir el Programa 
+//Compilación: g++ Archivo.cpp
+//Ejecución: ./a.out
+
+//Variables: int, float, double, char, string, bool
+//Operadores:
+//Funciones:
+//Estructura de Datos Básicos.
+
