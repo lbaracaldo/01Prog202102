c1795ea60607e2115a0b41a3f59c7725898ac8a1
Alejandro Baracaldo <lbaracaldo@unal.edu.co>
2021-11-08 12:52:00 -0500

    Trapezoidal Final

--- MoodleIntegralSimpsonTrabajoEnergía/TrabajoEnergía.cpp
+++ MoodleIntegralSimpsonTrabajoEnergía/TrabajoEnergía.cpp
@@ -5,20 +5,21 @@
 
 double Velocity(double x);
 template <typename fptr>
-double IntegralTrapezoidal1(double a,double b,double h, fptr f);
+double IntegralTrapezoidal1(double a,double b,int h, fptr f);
 
-int main(void){
+int main(int argc, char **argv){
   std::cout.precision(15); std::cout.setf(std::ios::scientific);
-  const double XMIN=0;
-  const double XMAX=4;
-  const double N1=10;
-  const double N2=1000;
-  const double h=0.4;
+  const double XMIN=std::atof(argv[1]);
+  const double XMAX=std::atof(argv[2]);
+  const int h=std::atoi(argv[3]);
   
-  const double IntegralExacta=std::sin(XMAX)*3.2-3.2*XMAX*sin(XMAX)-3.2*sin(XMIN)+3.2*XMIN*sin(XMIN);
- std::cout<<fabs(1-IntegralTrapezoidal1(XMIN,XMAX,h,Velocity)/IntegralExacta)<<"\n";
+  const double IntegralExacta=std::sqrt(sin(XMAX)*3.2-3.2*XMAX*cos(XMAX)-3.2*sin(XMIN)+3.2*XMIN*cos(XMIN));
+  std::cout<<IntegralExacta<<"\n";
 
-  std::cout<<IntegralTrapezoidal1(XMIN,XMAX,h, Velocity)<<"\n";
+std::cout<<std::sqrt(IntegralTrapezoidal1(XMIN,XMAX,h, Velocity))<<"\n";
+   
+  std::cout<<fabs(1-std::sqrt(IntegralTrapezoidal1(XMIN,XMAX,h,Velocity))/IntegralExacta)<<"\n";
+
   return 0;
 }
 
@@ -31,15 +32,16 @@ double Velocity(double x)
 }
 
 template <typename fptr>
-double IntegralTrapezoidal1(double a, double b, double h,fptr Velocity)
+double IntegralTrapezoidal1(double a, double b, int h,fptr Velocity)
 {
+  const double s=(b-a)/h;
   double result=0.0;
-  for(int jj=1; jj<=9;++jj){
-    double xj=jj*h;
+  for(int jj=1; jj<=h-1;++jj){
+    double xj=jj*s;
     result += Velocity(xj);
   }
   result+= 0.5*(Velocity(a)+Velocity(b));
-  result *= h;
+  result *= s;
   
   return result;
 }
